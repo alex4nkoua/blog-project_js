4515230107b237a5522d50fece0d2fe706e94180
Alexandre Nkoua <alexnkoua4code@gmail.com>
2025-08-24 09:37:53 +0200

    mise en place du JavaScript pour le formulaire

--- src/form/form.js
+++ src/form/form.js
@@ -1,3 +1,35 @@
 import "../assets/styles/styles.scss";
 import "/assets/javascript/topbar";
 import "./form.scss";
+
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const formData = new FormData(form);
+	const article = Object.fromEntries(formData.entries());
+	if (formIsValid(article)) {
+		const json = JSON.stringify(article);
+		// fetch
+	}
+});
+
+const formIsValid = (article) => {
+	if (!article.author || !article.Category || !article.content) {
+		errors.push("Vous devez renseigner tous les champs");
+	} else {
+		errors = [];
+	}
+	if (errors.length) {
+		let errorHTML = " ";
+		errors.forEach((e) => {
+			errorHTML += ` <li>${e}</li>`;
+		});
+		errorElement.innerHTML = errorHTML;
+	} else {
+		errorElement.innerHTML = " ";
+	}
+};
